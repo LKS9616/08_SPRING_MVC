1b4d56a70f9cb1276727d7970dea5b5f5614602b
lks9616 <lks609591@gmail.com>
2024-07-09 15:31:38 +0900

    커밋

---
+++ chap02-handler-method-lecture-source/src/test/java/com/ohgiraffers/handlermethodd/Chap02HandlerMethodLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.handlermethodd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap02HandlerMethodLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
