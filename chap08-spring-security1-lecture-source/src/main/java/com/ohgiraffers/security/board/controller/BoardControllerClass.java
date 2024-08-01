cff48a18ed1232e80d480c81e4957a49cfbca877
lks9616 <lks609591@gmail.com>
2024-08-01 18:11:48 +0900

    cm

---
+++ chap08-spring-security1-lecture-source/src/main/java/com/ohgiraffers/security/board/controller/BoardControllerClass.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.security.board.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+@Slf4j
+public class BoardControllerClass {
+    @GetMapping("/board")
+    public void board() {
+
+    }
+}
